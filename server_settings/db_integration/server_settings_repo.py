ab8c4429a994d2f26054dc6cd38fb6a72c285518
Davis-Software <davissoftware6@gmail.com>
2021-11-06 20:09:06 +0100

    fixed a bug

--- server_settings/db_integration/server_settings_repo.py
+++ server_settings/db_integration/server_settings_repo.py
@@ -1,12 +1,13 @@
-from server_settings.db_integration.server_settings import ServerSettings
+from server_settings.db_integration.server_settings import ServerSettings, engine
 from utils import sql_utils
 
 
 def get_setting(key):
-    setting = ServerSettings.query.filter_by(key=key).first()
-    if setting is None:
-        return setting
-    return setting.value
+    result = engine.engine.execute(f"SELECT value FROM `server_settings` WHERE `key` = '{key}'")  # Doing legacy query stuff because it wouldn't work on webservers any other way
+    setting = [row[0] for row in result]
+    if setting:
+        return setting[0]
+    return None
 
 
 def set_setting(key, value):
