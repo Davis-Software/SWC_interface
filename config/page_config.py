1e0c7867c8160c8e3b9c174835ec38fe1bb7ef18
Davis-Software <davissoftware6@gmail.com>
2022-01-27 19:32:08 +0100

    added teamspeak module to server page - ability to see server tree - ability to see users in channel

--- config/page_config.py
+++ config/page_config.py
@@ -39,7 +39,8 @@ dashboard_module_description = {
 
 
 server_page_modules = {
-    "dynmap": ["Software-City MC Map", "Explore Software City's map through a bird's eye view with DynMap.", False]
+    "dynmap": ["Software-City MC Map", "Explore Software City's map through a bird's eye view with DynMap.", False],
+    "teamspeak": ["Teamspeak Channel Tree", "Get an overview of the software city teamspeak channels.", False]
 }
 
 
