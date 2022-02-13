445734fa978cd4cd60cf473a78eb319eb14867b7
Davis-Software <davissoftware6@gmail.com>
2022-02-13 01:27:28 +0100

    some updates - added image manipulation options to image preview in cloud - added better mobile app compatibility for some dashboard modules

--- route_helpers/server_page_request_handler.py
+++ route_helpers/server_page_request_handler.py
@@ -1,9 +1,21 @@
 from __init__ import render_template
 from user_data import user_auth
 from route_helpers import server_page_module_adapter
+from utils import api_utils
+from utils.request_code import RequestCode
 
 
 def handle_arguments(request, session):
+    if request.args.get("modules") is not None and request.args.get("username") is not None:
+        return api_utils.craft_response(
+            server_page_module_adapter.get_modules(
+                user_auth.is_admin(
+                    request.args.get("username")
+                )
+            ),
+            RequestCode.Success.OK
+        )
+
     if request.args.get("get") is not None:
         return render_template(f"components/server/modules/{request.args.get('get')}.html")
 
---
+++ static/cloud_previews/js/image.js
@@ -0,0 +1,54 @@
+let loader = document.querySelector(".spinner-border")
+let img_elem = document.querySelector("#image")
+
+function set_loader(val) {
+    loader.hidden = !val
+    document.querySelectorAll("button[data-action]").forEach(elem => {
+        elem.disabled = val
+    })
+}
+function set_image(data) {
+    img_elem.src = data
+}
+
+IJS.Image.load(img_elem.src).then(image => {
+    img_elem.addEventListener("load", _ => {
+        set_loader(false)
+    })
+
+    document.querySelectorAll("button[data-action]").forEach(elem => {
+        elem.addEventListener("click", async function () {
+            set_loader(true)
+            setTimeout(_ => {
+                switch (elem.getAttribute("data-action")) {
+                    case "f_h":
+                        image = image.flipX()
+                        break
+                    case "f_v":
+                        image = image.flipY()
+                        break
+                    case "r_r":
+                        image = image.rotate(90)
+                        break
+                    case "r_l":
+                        image = image.rotate(-90)
+                        break
+                    case "dwn":
+                        image.toBlob("image/png", 1).then(blob => {
+                            download_blob(
+                                blob,
+                                location.pathname.split("/").pop(),
+                                blob.type
+                            )
+                        })
+                        break
+                    default:
+                        return
+                }
+                set_image(image.toDataURL())
+                set_loader(false)
+            }, 100)
+        })
+    })
+})
+
