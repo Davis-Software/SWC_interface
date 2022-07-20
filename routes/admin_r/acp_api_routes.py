6277302689cfa08b60394d0377398b44f92a2fd7
Davis-Software <davissoftware6@gmail.com>
2022-07-20 17:45:50 +0200

    added missing admin requirements

--- routes/admin_r/acp_api_routes.py
+++ routes/admin_r/acp_api_routes.py
@@ -5,10 +5,12 @@ from flask import render_template
 from __init__ import app
 import configuration
 from route_helpers.acp_api_route_adapter import acquire_api_functions, get_file_content
+from user_data.user_auth import admin_required
 
 
 @app.route('/acp/api/')
 @app.route("/acp/api/<path:path>")
+@admin_required
 def acp_api(path=None):
     if path is None:
         path = configuration.server_api_path
--- routes/admin_r/acp_controls_routes.py
+++ routes/admin_r/acp_controls_routes.py
@@ -2,11 +2,13 @@ from flask import render_template
 
 from __init__ import app
 from server_settings.server_control import PowerOptions
+from user_data.user_auth import admin_required
 from utils import api_utils
 
 
 @app.route("/acp/controls")
 @app.route("/acp/controls/<string:option>/<string:operation>")
+@admin_required
 def acp_controls(option=None, operation=None):
     if option == "power":
         if operation == "shutdown":
